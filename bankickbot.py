fd51e02339e0b45b27f5e364efbdbed61e0529de
Sundance <77976941+sundanc@users.noreply.github.com>
2021-03-16 12:28:43 +0300

    Update bankickbot.py

--- bankickbot.py
+++ bankickbot.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
+from keep_alive import keep_alive
 
-client = commands .AutoShardedBot(command_prefix=commands.when_mentioned_or('!'), help_command=None)
+client = commands .AutoShardedBot(command_prefix=commands.when_mentioned_or('!'), help_command=None) #prefix
 
 @client.event
 async def on_Ready():
@@ -12,7 +13,7 @@ async def ban(ctx, user: discord.User):
   guild = ctx.guild 
   mbed = discord.Embed(
     title = 'Successs!',
-    descsription = f"(user) kullanıcı başarıyla banlandı."
+    descsription = f"(user) has successfully been banned."
   )
   if ctx.author.guild_permissions.ban_members:
    await ctx.send(embed=mbed)
@@ -23,18 +24,45 @@ async def kick(ctx, user: discord.User):
   guild = ctx.guild 
   mbed = discord.Embed(
     title = 'Successs!',
-    descsription = f"(user) kullanıcı başarıyla kicklendi."
+    descsription = f"(user) has successfully been kicked."
   )
   if ctx.author.guild_permissions.kick_members:
    await ctx.send(embed=mbed)
    await guild.kick(user=user)
+
+@client.command(description="Mutes the specified user.")
+@commands.has_permissions(manage_messages=True)
+async def mute(ctx, member: discord.Member, *, reason=None):
+    guild = ctx.guild
+    mutedRole = discord.utils.get(guild.roles, name="Muted")
+
+    if not mutedRole:
+        mutedRole = await guild.create_role(name="Muted")
+
+        for channel in guild.channels:
+            await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=False)
+    embed = discord.Embed(title="muted", description=f"{member.mention} was muted ", colour=discord.Colour.light_gray())
+    embed.add_field(name="reason:", value=reason, inline=False)
+    await ctx.send(embed=embed)
+    await member.add_roles(mutedRole, reason=reason)
+    await member.send(f" you have been muted from: {guild.name} reason: {reason}")
+
+@client.command(description="Unmutes a specified user.")
+@commands.has_permissions(manage_messages=True)
+async def unmute(ctx, member: discord.Member):
+   mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
+
+   await member.remove_roles(mutedRole)
+   await member.send(f" you have unmutedd from: - {ctx.guild.name}")
+   embed = discord.Embed(title="unmute", description=f" unmuted-{member.mention}",colour=discord.Colour.light_gray())
+   await ctx.send(embed=embed)
 
 @client.command()
 async def unban(ctx, user: discord.User):
   guild = ctx.guild
   mbed = discord.Embed(
     title = 'Success!',
-    descsription = f"{user} kullanıcının banı açıldı."
+    descsription = f"{user} has successfully been unbanned."
   )
   if ctx.author.guild_permissions.ban_members:
     await ctx.send(embed=mbed)
